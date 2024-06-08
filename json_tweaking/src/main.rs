0d5dc3b58dfbbd5fd5a8cdcec8b534d097d14f38
DARKLORD513 <akshad.agrawal1978@gmail.com>
2024-06-08 21:47:40 +0530

    json write applied

--- json_tweaking/src/main.rs
+++ json_tweaking/src/main.rs
@@ -1,5 +1,6 @@
 use serde::{Deserialize, Serialize};
-use std::io;
+use serde_json::to_string_pretty;
+use std::io::{self, BufWriter, Write};
 use std::fs::File;
 use std::io::BufReader;
 use std::path::Path;
@@ -14,6 +15,15 @@ struct ApiInfo{
 fn main() -> Result<(), Box<dyn std::error::Error>>{
 
     let file_path = std::path::Path::new("test.json");
+
+    let data = ApiInfo {
+        api_key: "hfhfhsjjaanecienen".to_string(),
+        model: "llama3 70b".to_string(),
+    };
+
+    let _ = write_json(file_path, &data);
+
+    // ? reading the json file
     let extracted_info: ApiInfo= read_json(&file_path)?;
     println!("\nApi key: {}\nModel: {}", extracted_info.api_key, extracted_info.model);
 
@@ -26,4 +36,13 @@ fn read_json(file_path : &Path) -> Result<ApiInfo, io::Error>{
 
     let content = serde_json::from_reader(reader)?;
     Ok(content)
+}
+
+fn write_json(file_path : &Path, data : &ApiInfo) -> Result<(), io::Error>{
+    let file = File::create(file_path)?;
+    let mut writer = BufWriter::new(file);
+
+    let _ = serde_json::to_writer(&mut writer, &data);
+    let _ = writer.flush()?;
+    Ok(())
 }

e7ef611433d0dd993b7375d7c5d5de2516a5e97c
DARKLORD513 <akshad.agrawal1978@gmail.com>
2024-06-08 14:47:37 +0530

    removed args and added some error handling

--- File_Compression/src/main.rs
+++ File_Compression/src/main.rs
@@ -1,20 +1,45 @@
-extern crate flate2;
-
 use flate2::write::GzEncoder;
 use flate2::Compression;
-use std::env::args;
 use std::fs::File;
 use std::io::copy;
+use std::io;
 use std::io::BufReader;
 use std::time::Instant;
 
 fn main(){
-    if args().len() != 3{
-        eprintln!("Usage: source output");
-        return;
-    }
-    let mut input_file = BufReader::new(File::open(args().nth(1).unwrap()).unwrap());
-    let output_file = File::create(args().nth(2).unwrap()).unwrap();
+
+    println!("Enter the path to source file: ");
+    let mut source_path = String::new();
+    io::stdin().read_line(&mut source_path)
+        .expect("Failed to read the source path");
+
+    let source_path: String = source_path.trim().parse()
+        .expect("Failed to parse as string");
+
+        
+    println!("Enter the path to output file (also include the file name): ");
+    let mut output_path = String::new();
+    io::stdin().read_line(&mut output_path)
+        .expect("Failed to read the output path");
+
+    let output_path: String = output_path.trim().parse()
+    .expect("Failed to parse as string");
+
+    let mut input_file = match File::open(source_path) {
+        Ok(file) => BufReader::new(file),
+        Err(error) => {
+            println!("Failed to open file: {}", error);
+            return;
+        }
+    };
+
+    let output_file = match File::create(output_path) {
+        Ok(file) => file,
+        Err(error) => {
+            println!("Failed to create file: {}", error);
+            return;
+        }
+    };
 
     let mut result = GzEncoder::new(output_file, Compression::default());
     let start = Instant::now();

0f8a24a3fd62ac3c5034a08160ef5f4a4891ef5e
DARKLORD513 <akshad.agrawal1978@gmail.com>
2024-06-11 02:43:08 +0530

    trying http requests

--- http_requests/src/main.rs
+++ http_requests/src/main.rs
@@ -1,2 +1,23 @@
 use error_chain::error_chain;
-use std::io::Read;
+
+error_chain! {
+    foreign_links{
+        Io(std::io::Error);
+        HttpRequest(reqwest::Error);
+    }
+}
+
+#[tokio::main]
+async fn main()-> Result<()>{
+
+
+    // ? using a block request
+    // let req = reqwest::blocking::get("https://www.rust-lang.org")?;
+
+    // ? using async request
+    let req = reqwest::get("https://www.rust-lang.org").await?;
+    println!("{:?}", req.status());
+    println!("{:?}", req.headers());
+
+    Ok(())
+}
